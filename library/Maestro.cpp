b6cd9d9cb2160245e2e125893b84a335dcd56bf1
CreedyNZ <andrew@creahan.co.nz>
2015-08-16 20:37:24 +1200

    Maestro again

--- library/Maestro.cpp
+++ library/Maestro.cpp
@@ -1,111 +1,111 @@
-// Copyright (C) Pololu Corporation.  See LICENSE.txt for details.
+#include "maestro.h"
+#include "protocol.h"
 
-#include "Maestro.h"
+const uint16_t Maestro::vendorId = 0x1ffb;
+const size_t Maestro::nProductIds = 4;
+const uint16_t Maestro::productIds[] = { 0x0089, 0x008a, 0x008b, 0x008c };
 
-Maestro::Maestro(uint8_t resetPin,
-                 uint8_t deviceNumber,
-                 bool CRCEnabled)
-{
-  _deviceNumber = deviceNumber;
-  _resetPin = resetPin;
-  _CRCEnabled = CRCEnabled;
-}
+Maestro::Maestro() {
+  libusb_init(&ctx);
 
-void Maestro::reset()
-{
-  if (_resetPin != noResetPin)
-  {
-    digitalWrite(_resetPin, LOW);
-    pinMode(_resetPin, OUTPUT); // Drive low.
-    delay(1);
-    pinMode(_resetPin, INPUT); // Return to high-impedance input (reset is
-                               // internally pulled up on Maestro).
-    delay(200); // Wait for Maestro to boot up after reset.
+  libusb_device** list;
+  int count = libusb_get_device_list(ctx, &list);
+
+  // scan usb devices looking for a Micro Maestro
+  bool found = false;
+  for (int i = 0; i < count; i++) {
+    libusb_device* device = list[i];
+
+    // check if this is a known device
+    struct libusb_device_descriptor desc;
+    libusb_get_device_descriptor(device, &desc);
+    if (!isMaestroDevice(desc)) { continue; }
+    productId = desc.idProduct;
+
+    // attempt to open device
+    int ret = libusb_open(device, &deviceHandle);
+    throwIfError<MaestroInitializationError>(ret);
+
+    // found a matching device that succesfully opened
+    found = true;
+    break;
+  }
+
+  libusb_free_device_list(list, 1);
+
+  if (!found) {
+    // there is no open device handle, so no need to close it
+    libusb_exit(ctx);
+
+    // fail fast; more clear than trying to open devices in the background
+    throw NoMaestroFoundError();
   }
 }
 
-void Maestro::goHome()
-{
-  writeCommand(goHomeCommand);
-  writeCRC();
+Maestro::~Maestro() {
+  // libusb cleanup
+  libusb_close(deviceHandle);
+  libusb_exit(ctx);
 }
 
-void Maestro::setTarget(uint8_t channelNumber, uint16_t target)
-{
-  writeCommand(setTargetCommand);
-  write7BitData(channelNumber);
-  write14BitData(target);
-  writeCRC();
-}
-
-void Maestro::setSpeed(uint8_t channelNumber, uint16_t speed)
-{
-  writeCommand(setSpeedCommand);
-  write7BitData(channelNumber);
-  write14BitData(speed);
-  writeCRC();
-}
-
-void Maestro::setAcceleration(uint8_t channelNumber, uint16_t acceleration)
-{
-  writeCommand(setAccelerationCommand);
-  write7BitData(channelNumber);
-  write14BitData(acceleration);
-  writeCRC();
-}
-
-
-
-void Maestro::writeByte(uint8_t dataByte)
-{
-  _stream->write(dataByte);
-
-  if(_CRCEnabled)
-  {
-    _CRCByte ^= dataByte;
-    for (uint8_t j = 0; j < 8; j++)
-    {
-      if (_CRCByte & 1)
-      {
-        _CRCByte ^= CRC7Polynomial;
-      }
-      _CRCByte >>= 1;
+bool Maestro::isMaestroDevice(libusb_device_descriptor& desc) {
+  if (desc.idVendor != vendorId) { return false; }
+  for (int i = 0; i < nProductIds; i++) {
+    if (desc.idProduct == productIds[i]) {
+      // vendor and product both matched
+      return true;
     }
   }
+  // no product id match
+  return false;
 }
 
-void Maestro::writeCRC()
-{
-  if(_CRCEnabled)
-  {
-    _stream->write(_CRCByte);
-    _CRCByte = 0; // Reset CRCByte to initial value.
+int Maestro::controlTransfer(uint8_t type, uint8_t request, uint16_t value, uint16_t index) {
+  int ret = libusb_control_transfer(
+    deviceHandle, type, request,
+    value, index,
+    (unsigned char*) 0, 0, 5000);
+  return throwIfError<MaestroControlError>(ret);
+}
+
+template <class T>
+int Maestro::throwIfError(int code) {
+  if (code < 0) {
+    // code is an error, throw
+    T* err = new T(errorDescription(code));
+    throw err;
   }
+  return code;
 }
 
-void Maestro::writeCommand(uint8_t commandByte)
-{
-  if (_deviceNumber != deviceNumberDefault)
-  {
-    writeByte(baudRateIndication);
-    write7BitData(_deviceNumber);
-    write7BitData(commandByte);
-  }
-  else
-  {
-    writeByte(commandByte);
-  }
+std::string Maestro::errorDescription(int error) {
+  switch (error) {
+    case -1: return "I/O error.";
+    case -2: return "Invalid parameter.";
+    case -3: return "Access denied.";
+    case -4: return "Device does not exist.";
+    case -5: return "No such entity.";
+    case -6: return "Busy.";
+    case -7: return "Timeout.";
+    case -8: return "Overflow.";
+    case -9: return "Pipe error.";
+    case -10: return "System call was interrupted.";
+    case -11: return "Out of memory.";
+    case -12: return "Unsupported/unimplented operation.";
+    case -99: return "Other error";
+    default: return "Unknown error code.";
+  };
 }
 
-void Maestro::write7BitData(uint8_t data)
-{
-  writeByte(data & 0x7F);
+void Maestro::setTarget(uint8_t servo, uint16_t value) {
+  controlTransfer(0x40, REQUEST_SET_TARGET, value, servo);
 }
 
-void Maestro::write14BitData(uint16_t data)
-{
-  writeByte(data & 0x7F);
-  writeByte((data >> 7) & 0x7F);
+void Maestro::setSpeed(uint8_t servo, uint16_t value) {
+  controlTransfer(0x40, REQUEST_SET_SERVO_VARIABLE, value, servo);
 }
 
-
+void Maestro::setAcceleration(uint8_t servo, uint16_t value) {
+  // setting high bit on servo number indicates acceleration
+  controlTransfer(0x40, REQUEST_SET_SERVO_VARIABLE, value, servo | 0x80);
+}
--- library/Maestro.h
+++ library/Maestro.h
@@ -1,175 +1,117 @@
-// Copyright (C) Pololu Corporation.  See LICENSE.txt for details.
-
-/*! \file PololuMaestro.h
- *
- * This is the main header file for the Pololu Maestro Servo Controller library
- * for Arduino.
- *
- *
- * For an overview of the library's features, see
- * https://github.com/pololu/maestro-arduino. That is the main repository for
- * the library.
- */
-
 #pragma once
+#include <string>
+#include <stdexcept>
+#include <libusb-1.0/libusb.h>
 
-
-class Maestro
-{
-  public:
-    /** \brief The default device number, used to construct a MicroMaestro or
-        MiniMaestro object that will use the compact protocol.
-     */
-    static const uint8_t deviceNumberDefault = 255;
-
-    /** \brief The default reset pin is no reset pin, used to construct a
-        MicroMaestro or MiniMaestro object that will not have a reset pin.
-     */
-    static const uint8_t noResetPin = 255;
-
-    /** \brief Resets the Maestro by toggling the \p resetPin, if a \p resetPin
-     * was given.
-     *
-     * By default this function will do nothing. If the \p resetPin was
-     * specified while constructing the Maestro object, it will toggle that pin.
-     * That pin needs to be wired to the Maestro's RST pin for it to reset the
-     * servo controller.
-     */
-    void reset();
-
-    /** \brief Sets the \a target of the servo on \a channelNumber using the
-     * Mini SSC protocol.
-     *
-     * @param channelNumber A servo number from 0 to 254.
-     *
-     * @param target A target position from 0 to 254.
-     *
-     */
-    
-     */
-    void setTarget(uint8_t channelNumber, uint16_t target);
-
-    /** \brief Sets the \a speed limit of \a channelNumber.
-     *
-     * @param channelNumber A servo number from 0 to 127.
-     *
-     * @param speed A number from 0 to 16383.
-     *
-     * Limits the speed a servo channel’s output value changes.
-     *
-     * See the Serial Interface section in the [Maestro User's
-     * Guide](http://www.pololu.com/docs/0J40) for more details.
-     *
-     * The compact protocol is used by default. If the %deviceNumber was given
-     * to the constructor, it uses the Pololu protocol.
-     */
-    void setSpeed(uint8_t channelNumber, uint16_t speed);
-
-     /** \brief Sets the \a acceleration limit of \a channelNumber.
-     *
-     * @param channelNumber A servo number from 0 to 127.
-     *
-     * @param acceleration A number from 0 to 16383.
-     *
-     * Limits the acceleration a servo channel’s output value changes.
-     *
-     * See the Serial Interface section in the [Maestro User's
-     * Guide](http://www.pololu.com/docs/0J40) for more details.
-     *
-     * The compact protocol is used by default. If the %deviceNumber was given
-     * to the constructor, it uses the Pololu protocol.
-     */
-    void setAcceleration(uint8_t channelNumber, uint16_t acceleration);
-
-    /** \brief Sends the servos and outputs to home position.
-     *
-     * If the "On startup or error" setting for a servo or output channel is set
-     * to "Ignore", the position will be unchanged.
-     *
-     * See the Serial Interface section in the [Maestro User's
-     * Guide](http://www.pololu.com/docs/0J40) for more details.
-     *
-     * The compact protocol is used by default. If the %deviceNumber was given
-     * to the constructor, it uses the Pololu protocol.
-     */
-    void goHome();
-
-    /** \brief Stops the script.
-     *
-     * Stops the script, if it is currently running.
-     *
-     * See the Serial Interface section in the [Maestro User's
-     * Guide](http://www.pololu.com/docs/0J40) for more details.
-     *
-     * The compact protocol is used by default. If the %deviceNumber was given
-     * to the constructor, it uses the Pololu protocol.
-     */
-    
-    uint16_t getPosition(uint8_t channelNumber);
-
-    /** \brief Gets the moving state for all configured servo channels.
-     *
-     * @return 1 if at least one servo limited by speed or acceleration is still
-     * moving, 0 if not.
-     *
-     * Determines if the servo outputs have reached their targets or are still
-     * changing and will return 1 as as long as there is at least one servo that
-     * is limited by a speed or acceleration setting.
-     *
-     * See the Serial Interface section in the [Maestro User's
-     * Guide](http://www.pololu.com/docs/0J40) for more details.
-     *
-     * The compact protocol is used by default. If the %deviceNumber was given
-     * to the constructor, it uses the Pololu protocol.
-     */
-    uint8_t getMovingState();
-
-    /** \brief Gets if the script is running or stopped.
-     *
-     * @return 1 if script is stopped, 0 if running.
-     *
-     * The compact protocol is used by default. If the %deviceNumber was given
-     * to the constructor, it uses the Pololu protocol.
-     */
-    
-    uint16_t getErrors();
-
-    /** \cond
-    *
-    * This should be considered a private implementation detail of the library.
-    **/
-  protected:
-    Maestro(uint8_t resetPin,
-            uint8_t deviceNumber,
-            bool CRCEnabled);
-
-    void writeByte(uint8_t dataByte);
-    void writeCRC();
-    void writeCommand(uint8_t commandByte);
-    void write7BitData(uint8_t data);
-    void write14BitData(uint16_t data);
-  /** \endcond **/
-
-  private:
-    static const uint8_t CRC7Polynomial = 0x91;
-    static const uint8_t baudRateIndication = 0xAA;
-
-    static const uint8_t miniSscCommand = 0xFF;
-    static const uint8_t setTargetCommand = 0x84;
-    static const uint8_t setSpeedCommand = 0x87;
-    static const uint8_t setAccelerationCommand = 0x89;
-    static const uint8_t getPositionCommand = 0x90;
-    static const uint8_t getMovingStateCommand = 0x93;
-    static const uint8_t getErrorsCommand = 0xA1;
-    static const uint8_t goHomeCommand = 0xA2;
-    
-
-    uint8_t _deviceNumber;
-    uint8_t _resetPin;
-    bool _CRCEnabled;
-    uint8_t _CRCByte;
+/**
+ * Thrown when no valid Maestro device is found
+ */
+class NoMaestroFoundError : public std::runtime_error {
+public:
+    NoMaestroFoundError() : std::runtime_error("NoMaestroFoundError") { }
 };
 
+/**
+ * Thrown when a valid device is found but can not be opened or otherwise initialized.
+ */
+class MaestroInitializationError : public std::runtime_error {
+public:
+    MaestroInitializationError(std::string what) : std::runtime_error(what) { }
+    MaestroInitializationError(char* what) : std::runtime_error(what) { }
+};
 
+/**
+ * Thrown when a control command to an already open device fails.
+ */
+class MaestroControlError : public std::runtime_error {
+public:
+    MaestroControlError(std::string what) : std::runtime_error(what) { }
+    MaestroControlError(char* what) : std::runtime_error(what) { }
+};
 
+/**
+ * Wrapper class for a Pololu Micro Maestro.
+ * Uses libusb-1.0 to establish a connection and abtracts the basic control.
+ */
+class Maestro {
+private:
 
+    /**
+     * vendor id for all maestro devices
+     */
+    static const uint16_t vendorId;
+    
+    /**
+     * number of product ids
+     */
+    static const size_t nProductIds;
+    
+    /**
+     * array of all valid product ids for maestro devices
+     */
+    static const uint16_t productIds[];
+
+    /**
+     * Given a device descriptor, determine if it's a Maestro device.
+     */
+    static bool isMaestroDevice(libusb_device_descriptor& desc);
+
+    /**
+     * product id for the device this instance has a handle to
+     */
+    uint16_t productId;
+
+    libusb_context* ctx;
+    libusb_device_handle* deviceHandle;
+
+    /**
+     * Helper method, calls controlTransfer on the underlying device handle.
+     * @see libusb_control_transfer
+     */
+    int controlTransfer(uint8_t type, uint8_t request, uint16_t value, uint16_t index);
+
+    /**
+     * Throw if the passed return code is an error (< 0), otherwise just pass it through
+     * @param code return code
+     * @tparam T error type, needs a constructor with the signature T(std::string)
+     */
+    template <class T>
+    static int throwIfError(int code);
+
+    /**
+     * Return error message associated with Maestro return codes.
+     */
+    static std::string errorDescription(int error);
+
+public:
+    /**
+     * Basic constructor, will open the first valid device it finds.
+     */
+    Maestro();
+
+    /**
+     * Close underlying libusb handle, clean up.
+     */
+    ~Maestro();
+
+    /**
+     * Set the target pulse width for a particular servo. The PWM signal will gradually approach the target value based on the curent speed setting.
+     * @param servo 0-indexed servo number.
+     * @param value Pulse width in 0.25µs increments (e.g. for 1000µs you should pass 4000)
+     */
+    void setTarget(uint8_t servo, uint16_t value);
+
+    /**
+     * Set the maximum travel speed. This controls how quickly the PWM signal will move to the target value.
+     * @param servo 0-indexed servo number.
+     * @param value Maximun pulse width change rate, in 0.25µs/10ms
+     */
+    void setSpeed(uint8_t servo, uint16_t value);
+
+    /**
+     * Set the maximum acceleration.
+     * @param servo 0-indexed servo number.
+     * @param value Has a value in range (0..255) with units 0.25µs/10ms/80ms
+     */
+    void setAcceleration(uint8_t servo, uint16_t value);
+};
